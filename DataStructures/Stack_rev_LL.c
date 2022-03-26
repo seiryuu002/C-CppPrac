9d310fcdf0a98a508a31936bf9e4a83ac68edaec
Apurv Kumar <kuroseiryuu025@gmail.com>
2022-03-26 21:54:23 +0530

    update

--- DataStructures/Stack_rev_LL.c
+++ DataStructures/Stack_rev_LL.c
@@ -1,27 +1,38 @@
 #include<stdio.h>
 #include <stdlib.h>
-// #define max 101
-// struct node* stack[max];
+#define max 101
+struct node* stack[max];
+int elements = 0;
+int top = -1;
 
 struct node{
     int data;
     struct node* next;
 };
-struct node* top = NULL;
+struct node* head = NULL;
 
 void insert(int data){
     struct node* temp = (struct node*)malloc(sizeof(struct node*));
     temp->data = data;
     temp->next = NULL;
-    struct node* temp2 = top;
-    while(temp2 -> next != NULL){
-        temp2 = temp2 -> next;
+    if(head == NULL)
+    {
+        head = temp;
     }
-    temp2->next = temp;
+    else{
+        struct node *temp2 = head;
+        while (temp2->next != NULL)
+        {
+            temp2 = temp2->next;
+        }
+        temp2->next = temp;
+    }
+    elements++;
+
 }
 
 void print(){
-    struct node* temp = top;
+    struct node* temp = head;
     printf("LL: Head ->");
     while(temp != NULL){
         printf(" %d ", temp->data);
@@ -30,14 +41,62 @@ void print(){
     printf("\n");
 }
 
-// void push(){
-//     struct node* temp = top;
-//     while(temp != NULL){
-//         push();
-//         temp = temp -> next;
-//      }
-// }
+void print_stack()
+{
+    int i;
+    printf("Stack: ");
+    for (i = 0; i <= top; i++)
+    {
+        printf(" %d ", stack[i]);
+    }
+    printf("\n");
+}
 
+void push(){
+    if (top == max - 1)
+    {
+        printf("Stack Overflow");
+        return;
+    }
+    struct node* temp = head;
+    while(temp != NULL){
+        stack[++top] = temp;
+        temp = temp->next;
+    }
+}
+
+void pop()
+{
+    if (top == -1)
+    {
+        printf("Error: No element to pop\n");
+        return;
+    }
+    top--;
+}
+
+struct node* Top(){
+    return stack[top];
+}
+
+int is_empty(){
+    if(top == -1)
+    return 1;
+    else
+    return 0;
+}
+
+void reverse(){
+    struct node* temp = Top();
+    head = temp;
+    pop();
+    while(!is_empty()){
+        temp->next = Top();
+        pop();
+        temp = temp -> next;
+    }temp-> next = NULL;
+
+}
 
 void main(){
     insert(2);
@@ -45,5 +104,10 @@ void main(){
     insert(6);
     insert(8);
     insert(10);
+    print();
+    push();
+    print_stack();
+    printf("Top: %d\n", Top());
+    reverse();
     print();
 }
