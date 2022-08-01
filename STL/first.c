0c0ba229dbb551226c33e63df7727b3e5a7fc806
Apurv Kumar <kuroseiryuu025@gmail.com>
2022-08-01 22:30:26 +0530

    update

---
+++ STL/first.c
@@ -0,0 +1,12 @@
+// STL Introduction(standard template library)
+/* It provides implementaation of basic data structures so that
+ we don't need to implement whole data structure from scratch 
+ STL in Cpp can be divided into two parts 1.Containers 2.Algorithms*/
+// Containers -> it is implementation of basic data structures
+/* Containers can be further divide into three types of containers
+    1.Simple ->            pair, vector, forward_list, list
+    2.Container adapters-> stack, queue, priority_queue
+    3.Associative ->       set, map, unordered_set, unordered_map*/
+// Algorithms -> are implementation of basic algorithms.
+// ex. -> binary_search, find, reverse, sort, etc.
+
