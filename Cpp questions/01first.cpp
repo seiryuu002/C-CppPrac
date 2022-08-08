e2f58a554b7a7278615a089cf58dae8e2852bc4a
Apurv Kumar <kuroseiryuu025@gmail.com>
2022-08-08 13:13:12 +0530

    update

---
+++ Cpp questions/01first.cpp
@@ -0,0 +1,25 @@
+/*  This file covers basics of C++ programming just to refresh my memory */
+/*  When a person starts programming everyone's first piece of code is to 
+    print hello world, its like to depict that you have entered a new world 
+    and what do you say to someone you meet...yes exactly you say "Hello" 
+    So, first exercise is <1> WAP to print 'Hello World on the terminal'*/
+
+#include<iostream>
+using namespace std;
+
+// First program 
+int main(){
+    cout<<"Hello World";
+    return 0;
+}
+
+//Congrats let's move to next one...
+// few Data types
+/*     Type     	       Keyword
+      Boolean               bool
+     Character 	            char
+      Integer 	             int
+  Floating point 	        float
+Double floating point 	    double
+     Valueless 	             void
+   Wide character 	        wchar_t*/
