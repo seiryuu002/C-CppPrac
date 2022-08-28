f77e95525a14c375a16a8d2ac8d597f2828d37a6
Apurv Kumar <kuroseiryuu025@gmail.com>
2022-08-28 15:16:23 +0530

    update

--- OOPs in CPP/2_Constructor.cpp
+++ OOPs in CPP/2_Constructor.cpp
@@ -1,13 +1,13 @@
 /**************************************************************************
  *                             CONSTRUCTORS in OOPs
- *
+ * In C++, constructor is a special method which is invoked automatically at the time of object creation. It is used to initialize the data members of new object generally. The constructor in C++ has the same name as class or structure.
  * There are two types 1.Default, 2.Parameterized
  * 1.   Default:    A constructor which has no argument is known as default
- *                  constructor.  It  is  invoked  at  the time of creating 
+ *                  constructor.  It  is  invoked  at  the time of creating
  *                  object.
- * 
- * 2.Parameterized: A  constructor  which  has  parameters is called 
- *                  parameterized constructor. It is used to provide 
+ *
+ * 2.Parameterized: A  constructor  which  has  parameters is called
+ *                  parameterized constructor. It is used to provide
  *                  different values to distinct objects.
  **************************************************************************/
 #include<iostream>
