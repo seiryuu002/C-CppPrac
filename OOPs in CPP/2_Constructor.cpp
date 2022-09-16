05eb272e6ef41bc4092ef4fa613c739e25b6200f
Apurv Kumar <kuroseiryuu025@gmail.com>
2022-09-17 01:26:50 +0530

    UPDATE

--- OOPs in CPP/2_Constructor.cpp
+++ OOPs in CPP/2_Constructor.cpp
@@ -5,14 +5,16 @@
  * members  of  new object generally. The constructor in C++ has the same 
  * name as class or structure.
  * 
- * There are two types 1.Default, 2.Parameterized
- * 1.   Default:    A constructor which has no argument is known as default
+ * There are three types 1.Default, 2.Parameterized, 3.Copy
+ * 1.Default:       A constructor which has no argument is known as default
  *                  constructor.  It  is  invoked  at  the time of creating
  *                  object.
  *
  * 2.Parameterized: A  constructor  which  has  parameters is called
  *                  parameterized constructor. It is used to provide
  *                  different values to distinct objects.
+ * 
+ * 3.Copy:          
  **************************************************************************/
 #include<iostream>
 #include<string>
@@ -20,22 +22,85 @@ using namespace std;
 
 class Student{
     public: 
-    int Id;
-    string Name;
+    int a;
+    int b;
     Student(){                                    // Default Constructor
         cout<<"Default constructor invoked"<<endl;
+        a = 10;
+        b = 20;
     }
-    Student(int id, string name){                 // Parameterized Constructor
-        Id = id;
-        Name = name;
+    Student(int n1,int n2){                 // Parameterized Constructor
+        a = n1;
+        b = n2;
     }
 };
 
-int main(){
-    Student S;
-    Student S2 = Student(1, "a");
-    cout<<"\n"<<S2.Id<<"\t"<<S2.Name<<endl;
+// int main(){
+//     Student S;
+//     cout<<"a: "<<S.a<<"\t"<<"b: "<<S.b<<endl;
+//     Student S2(5, 10);
+//     cout<<"\n"<<S2.a<<"\t"<<S2.b<<endl;
+//     return 0;
+// }
+
+// Copy Constructor
+// IMPLICIT COPY CONSTRUCTOR
+class Sample
+{
+    int id;
+
+public:
+    void init(int x)
+    {
+        id = x;
+    }
+    void display()
+    {
+        cout << endl
+             << "ID=" << id;
+    }
+};
+
+int main()
+{
+    Sample obj1;
+    obj1.init(10);
+    obj1.display();
+
+    Sample obj2(obj1); // or obj2=obj1;
+    obj2.display();
     return 0;
 }
 
+//EXPLICIT COPY CONSTRUCTOR
+// class Sample
+// {
+//     int id;
 
+// public:
+//     void init(int x)
+//     {
+//         id = x;
+//     }
+//     Sample() {} // default constructor with empty body
+
+//     Sample(Sample &t) // copy constructor
+//     {
+//         id = t.id;
+//     }
+//     void display()
+//     {
+//         cout << endl
+//              << "ID=" << id;
+//     }
+// };
+// int main()
+// {
+//     Sample obj1;
+//     obj1.init(10);
+//     obj1.display();
+
+//     Sample obj2(obj1); // or obj2=obj1;    copy constructor called
+//     obj2.display();
+//     return 0;
+// }
