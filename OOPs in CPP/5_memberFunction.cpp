f677d7a759b08dbfb9eec2ff5a2713bea09156f6
Apurv Kumar <kuroseiryuu025@gmail.com>
2022-09-16 21:06:41 +0530

    update

--- OOPs in CPP/5_memberFunction.cpp
+++ OOPs in CPP/5_memberFunction.cpp
@@ -15,13 +15,21 @@ using namespace std;
 class Cube{
     int side = 5;
     public:
-    int getVolume(){ return side*side*side;} // getVolume is a Simple function
+    int getVolume(){ return side*side*side;} // getVolume is a Simple function.
+    static int getVol(){                     // static function can only use static variables or 
+        int a = 6;                           // Variables declared within function.
+        return a*a*a; }
+    int getV() const{                        //const function.
+        int a = 7;
+        return a+a;
+    }
 };
-
 
 
 int main(){
     Cube c;
-    cout<<c.getVolume()<<endl;  // calling simple function
+    cout<<c.getVolume()<<endl;  // calling simple function.
+    cout<<Cube::getVol()<<endl; //calling static function.
+    cout<<c.getV()<<endl;       //calling const function
     return 0;
 }
