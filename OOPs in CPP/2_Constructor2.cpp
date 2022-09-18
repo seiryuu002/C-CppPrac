ca881240093ee96e7a3211bcbc1063622d5e48e4
Apurv Kumar <kuroseiryuu025@gmail.com>
2022-09-18 13:19:32 +0530

    up

--- OOPs in CPP/2_Constructor2.cpp
+++ OOPs in CPP/2_Constructor2.cpp
@@ -1,3 +1,27 @@
+/*************************************************************************
+* Shallow Copy Vs Deep Copy
+*
+**************************************************************************/
 #include<iostream>
 using namespace std;
+//Copy elision
 
+class Sample{
+    public:
+    void display()
+    {
+        cout<<"hello Dear!";        
+    }
+};
+
+void copyElision(){
+    Sample S;
+    for(int i = 0; i <= 2; i++ ){
+        S.display();
+        cout<<endl;
+    }
+}
+
+int main(){
+copyElision();
+}
