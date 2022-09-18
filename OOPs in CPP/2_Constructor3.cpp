56b3f8877fc1cb143149518ae1512d184fd76f06
Apurv Kumar <kuroseiryuu025@gmail.com>
2022-09-18 20:17:42 +0530

    update

--- OOPs in CPP/2_Constructor3.cpp
+++ OOPs in CPP/2_Constructor3.cpp
@@ -2,3 +2,10 @@
  * Virtual Constructor / Virtual Copy Constructor
  *
  **************************************************************************/
+#include <iostream>
+using namespace std;
+
+class Example{
+};
+
+int main(){}
