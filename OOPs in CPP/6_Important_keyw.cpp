61489a0abd27b89b7a02bb8f20809d26864c571d
Apurv Kumar <kuroseiryuu025@gmail.com>
2022-09-19 19:27:18 +0530

    up

--- OOPs in CPP/6_Important_keyw.cpp
+++ OOPs in CPP/6_Important_keyw.cpp
@@ -1,3 +1,10 @@
 /*********************************************************
- * STATIC KEYWORD
+ * STATIC KEYWORD-
+ * Static variables have a property of preserving their 
+ * value even after they are out of their scope! Hence, 
+ * static variables preserve their previous value in 
+ * their previous scope and are not initialized again in
+ * the new scope.
+ * 
+ * 
  *********************************************************/
