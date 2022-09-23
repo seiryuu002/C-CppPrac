fd8af51432612bebf4a9254975a8286c0149c1c9
Apurv Kumar <kuroseiryuu025@gmail.com>
2022-09-23 17:24:09 +0530

    up

--- OOPs in CPP/15_Inheritance1.cpp
+++ OOPs in CPP/15_Inheritance1.cpp
@@ -1,3 +1,23 @@
 /***************************************************************************
- *         INHERITANCE and its TYPES. 
+ *         INHERITANCE and its TYPES.
+ *
+ * Inheritance - In C++, inheritance is a process in which one object
+ * acquires all the properties and behaviors of its parent object
+ * automatically. In such way, you can reuse, extend or modify the
+ * attributes and behaviors which are defined in other class.
+ *
+ * In C++, the class which inherits the members of another class is 
+ * called derived class and the class whose members are inherited is 
+ * called base class. The derived class is the specialized class for 
+ * the base class.
+ * 
+ * 
+ * 
  ***************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+class Parent{
+
+}
