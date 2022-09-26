cc11524bd1b192acd44ba8f95bfd007c2bf01c8b
Apurv Kumar <kuroseiryuu025@gmail.com>
2022-09-26 23:22:27 +0530

    up

--- OOPs in CPP/16_Encapsulation.cpp
+++ OOPs in CPP/16_Encapsulation.cpp
@@ -1,3 +1,21 @@
 #include<iostream>
 using namespace std;
 
+class Encapsultion{
+    private:
+        int x;
+    public:
+        void set(int a){
+            x = a;
+        }
+        int get(){
+            return x;
+        } 
+};
+
+int main(){
+    Encapsultion obj;
+    obj.set(5);
+    cout<<obj.get();
+    return 0;
+}
