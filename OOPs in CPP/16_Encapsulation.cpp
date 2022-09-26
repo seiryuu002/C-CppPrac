e2eb9429ff5864236f8b47bb8070a70fb5cc5b86
Apurv Kumar <kuroseiryuu025@gmail.com>
2022-09-26 23:28:34 +0530

    up

--- OOPs in CPP/16_Encapsulation.cpp
+++ OOPs in CPP/16_Encapsulation.cpp
@@ -1,3 +1,17 @@
+/**************************************************************************
+ * ENCAPSULATION - In normal terms Encapsulation is defined as wrapping up
+ * of data and information under a single unit. In Object Oriented Programming,
+ * Encapsulation is defined as binding together the data and the functions
+ * that manipulates them.
+ *
+ * Example: Consider a real life example of encapsulation, in a company 
+ * there are different sections like the accounts section, finance section, 
+ * sales section etc. The finance section handles all the financial 
+ * transactions and keep records of all the data related to finance. 
+ * Similarly the sales section handles all the sales related activities 
+ * and keep records of all the sales.
+ *
+ **************************************************************************/
 #include<iostream>
 using namespace std;
 
