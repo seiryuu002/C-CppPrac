658d8011ee7d684934d98802dd229394223eba98
Apurv Kumar <kuroseiryuu025@gmail.com>
2022-09-29 17:28:32 +0530

    up

--- OOPs in CPP/17_Polymorphism.cpp
+++ OOPs in CPP/17_Polymorphism.cpp
@@ -15,3 +15,36 @@
  * Compile Time Polymorphism - This type of polymorphism is achieved by 
  *                             function overloading or operator overloading.
  *********************************************************************************/
+#include<iostream>
+using namespace std;
+
+/* FUNCTION OVERLOADING - When there are multiple functions with the same 
+   name but different parameters, then the functions are said to be 
+   overloaded, hence this is known as Function Overloading. Functions can 
+   be overloaded by changing the number of arguments or/and changing the 
+   type of arguments.
+*/
+class Poly{
+    public:
+
+    void func(int x){                         // Func is an overloaded function
+        cout<<"Value of x is "<<x<<endl;
+    }
+
+    void func(double x){
+        cout<<"value of x is "<<x<<endl;
+    }
+
+    void func(int x, int y){
+        cout<<"Value of x and y is "<<x<<", "<<y<<endl;
+    }
+};
+
+
+int main(){
+    Poly p;
+    p.func(1);              // Function Overloading
+    p.func(1.7326);
+    p.func(10, 5);
+    return 0;
+}
